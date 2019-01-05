dfafed1cd2350ec92578a528e6d3a449f8c46837
aarkoub <mofumofu@hotmail.fr>
2019-01-05 21:58:59 +0100

    Blockchain avec money typique

---
+++ src/main/TransactionInput.java
@@ -0,0 +1,24 @@
+package main;
+
+public class TransactionInput {
+	
+	private String transactionOutputId; //Reference to TransactionOutputs -> transactionId
+	private TransactionOutput UTXO; //Contains the Unspent transaction output
+	
+	public TransactionInput(String transactionOutputId) {
+		this.transactionOutputId = transactionOutputId;
+	}
+	
+	public String getTransactionOutputId(){
+		return transactionOutputId;
+	}
+	
+	public TransactionOutput getUTXO(){
+		return UTXO;
+	}
+	
+	public void setUTXO(TransactionOutput utxo){
+		this.UTXO = utxo;
+	}
+	
+}
