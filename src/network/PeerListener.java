51593cc2cb3c73a8263ede6d8483c45745f00453
L-C <lingchun.so@gmail.com>
2019-02-02 21:27:46 +0100

    ...

--- src/network/PeerListener.java
+++ src/network/PeerListener.java
@@ -7,7 +7,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
-import org.bouncycastle.crypto.engines.IndexGenerator.BitString;
 import org.json.JSONObject;
 
 import com.google.gson.JsonObject;
